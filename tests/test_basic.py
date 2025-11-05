27a30e5301d50edc2ae373e8a0de3a2d53fa49c3
Connor <connorware@outlook.com>
2025-11-05 22:08:45 +0000

    Add login negative test, remove time.sleep

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,9 +5,17 @@ import time
 def test_login(page, base_url):
     lp = LoginPage(page, base_url)
     lp.open()
-    
     lp.login("tomsmith", "SuperSecretPassword!")
-    time.sleep(10)
+    # assertions to confirm test login successful
     expect(page).to_have_url(f"{base_url}/secure")
+
+def test_wrong_login(page, base_url):
+    # Test incorrect password scenario
+    lp = LoginPage(page, base_url)
+    lp.open()
+    lp.login("tomsmith","abcdef")
+    # Check invalid text - inside elemet with id "flash"
+    expect(page.locator("#flash")).to_contain_text("Your password is invalid!")
+    
     
     
